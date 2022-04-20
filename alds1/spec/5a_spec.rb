6ca39b87c2273bd226826f19c9f9681ff7e52f4a
Naoki Oketani <okepy.naoki@gmail.com>
2022-04-20 19:46:03 +0900

    中級編 全探索：ビット全探索 10

---
+++ alds1/spec/5a_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe 'test' do
+  it 'test with "5\n1 5 7 10 21\n4\n2 4 17 8\n"' do
+    io = IO.popen('ruby alds1/5a.rb', 'w+')
+    io.puts("5\n1 5 7 10 21\n4\n2 4 17 8\n")
+    io.close_write
+    expect(io.readlines.join).to eq("no\nno\nyes\nyes\n")
+  end
+end
